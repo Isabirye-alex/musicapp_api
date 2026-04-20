2ba16fc22cfff87aac5b93c468ec9b3e5bb0c131
Isabirye-alex <alexisabirye558@gmail.com>
2026-04-20 10:32:47 +0300

    debugging

--- app/db/session.py
+++ app/db/session.py
@@ -14,9 +14,7 @@ DB_NAME = os.getenv("DB_NAME")
 if not all([DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME]):
     raise RuntimeError("Database environment variables are not fully set")
 
-URL = (
-    f"postgres://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?sslmode=require"
-)
+URL = f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}?sslmode=require"
 
 # DATABASE_URL = (
 #     f"postgresql+psycopg2://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
