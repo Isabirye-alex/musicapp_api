fd66d631d858d8ea7af99fd149f7076b99e8b31c
Isabirye-alex <alexisabirye558@gmail.com>
2026-04-25 19:30:12 +0300

    debugging

--- app/crud/songs/song_fetch_crud.py
+++ app/crud/songs/song_fetch_crud.py
@@ -4,6 +4,7 @@ from app.db.session import get_db
 from app.middleware.auth_middleware import auth_middleware
 from app.models.song_model import SongModel
 from app.models.favorite_songs_model import FavoriteSongsModel
+from sqlalchemy import bool_or
 
 
 def fetch_all_user_songs(db: Session, user_dict: dict):
@@ -24,23 +25,20 @@ def fetch_all_user_songs(db: Session, user_dict: dict):
     return songs
 
 
-def fetch_all_platform_songs(db: Session, user_dict: dict):
-    songs = db.query(SongModel).all()
+def fetch_all_platform_songs(db: Session, user_dict: dict | None):
+    user_id = user_dict.get("id") if user_dict else None
 
-    # Add is_favorite field for each song
-    if user_dict:
-        for song in songs:
-            favorite = (
-                db.query(FavoriteSongsModel)
-                .filter(
-                    FavoriteSongsModel.user_id == user_dict["id"],
-                    FavoriteSongsModel.song_id == song.song_id,
-                )
-                .first()
-            )
-            setattr(song, "is_favorite", favorite is not None)
-    else:
-        for song in songs:
-            setattr(song, "is_favorite", False)
+    query = db.query(
+        SongModel, (FavoriteSongsModel.user_id == user_id).label("is_favorite")
+    ).outerjoin(
+        FavoriteSongsModel,
+        (FavoriteSongsModel.song_id == SongModel.song_id)
+        & (FavoriteSongsModel.user_id == user_id),
+    )
 
-    return songs
+    results = query.all()
+
+    for song, is_fav in results:
+        song.is_favorite = bool(is_fav)
+
+    return [r[0] for r in results]
