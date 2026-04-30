c1903ca1cad32d5e30394cfeaf509b44bbe9caea
Isabirye-alex <alexisabirye558@gmail.com>
2026-04-30 14:41:40 +0300

    m

--- app/api/v1/endpoints/auth/sign_up.py
+++ app/api/v1/endpoints/auth/sign_up.py
@@ -25,14 +25,12 @@ def sign_up(
 
     try:
         new_user = create_user(db, user)
-        print("Sending Email........")
         background_tasks.add_task(
             send_registration_email,
             new_user.email,
             new_user.first_name,
             new_user.last_name,
         )
-        print("Email Sent........")
 
         return {"status": "success", "message": "User created successfully"}
 
